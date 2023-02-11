561053266c607f62b70ace42cbea8bd19812ec8d
shariquenadim <66444722+shariquenadim@users.noreply.github.com>
2023-02-11 11:58:53 +0530

    done

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  console.log("Hii");
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    console.log(tabs);
-  });
-});
+// chrome.tabs.onUpdated.addListener((tabId, tab) => {
+//   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+//     console.log(tabs[0].url);
+//   });
+// });
+chrome.webRequest.onCompleted.addListener(
+  function (details) {
+    console.log(details);
+  },
+  { urls: ["<all_urls>"] },
+  ["responseHeaders"]
+);

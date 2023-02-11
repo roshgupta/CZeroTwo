882deaa0d0726894f2631310cd308e053750f5a4
Roshan Gupta <gpta.duke.rohan.1219@gmail.com>
2023-02-11 18:57:36 +0530

    added more styles and made it functional

--- extensions/background.js
+++ extensions/background.js
@@ -1,36 +1,36 @@
-let total=0
-let vis=[{url:"default",value:0}]
+let total = 0
+let vis = [{ url: "default", value: 0 }]
 let curr_url;
 let curr_value;
 
 chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
-  curr_url="default";
-  curr_value=0;
+  curr_url = "default";
+  curr_value = 0;
   const url = new URL(tab.url);
-  curr_url=url.hostname;
+  curr_url = url.hostname;
 }
 );
 
 
 chrome.webRequest.onCompleted.addListener(
   function (details) {
-    for(let it of details.responseHeaders){
-      if(it.name==='content-length'){
+    for (let it of details.responseHeaders) {
+      if (it.name === 'content-length') {
         console.log(it.value)
-        total=total+parseInt(it.value);
-        curr_value=parseInt(it.value);
+        total = total + parseInt(it.value);
+        curr_value = parseInt(it.value);
       }
     }
 
-    let f=1;
-    for(let ind of vis){
-      if(ind.url===curr_url){
-        f=0;
-        ind.value=parseInt(ind.value)+parseInt(curr_value);
+    let f = 1;
+    for (let ind of vis) {
+      if (ind.url === curr_url) {
+        f = 0;
+        ind.value = parseInt(ind.value) + parseInt(curr_value);
       }
     }
-    if(f==1){
-      vis.push({url:curr_url,value:curr_value})
+    if (f == 1) {
+      vis.push({ url: curr_url, value: curr_value })
     }
 
     console.log(vis)
@@ -45,7 +45,9 @@ chrome.webRequest.onCompleted.addListener(
 
 chrome.windows.onRemoved.addListener((windowId) => {
   console.log("Closed window: " + windowId);
-  
+
   // Make an API request here
+
+  // after that clear local storage
 
 });
--- extensions/popup.js
+++ extensions/popup.js
@@ -1,5 +1,50 @@
-let visitedArrayData;
+// 1.02445483e-8
 chrome.storage.local.get(["visited"]).then((result) => {
-  visitedArrayData = result.visited;
-  console.log("Value currently is " + result.visited);
+  let visitedArrayData = JSON.parse(result.visited);
+  let template = "";
+  let count = 0;
+  let totalData = 0;
+  visitedArrayData.forEach(e => {
+    if (e && e.value) {
+
+      totalData += e.value;
+    }
+  });
+  const totalElement = document.getElementsByClassName("total-usages-value")[0];
+  totalElement.innerHTML = totalData;
+  for (let i = visitedArrayData.length - 1; i >= 0; i--) {
+    if (visitedArrayData[i] && visitedArrayData[i] != "default"
+      && visitedArrayData[i] != "undefined" && visitedArrayData[i] != undefined) {
+      count++;
+      template += `<li class="website">
+      <p class="url-link"> ${visitedArrayData[i].url}</p>
+      <div>
+        <div class="data-usages">
+          Data used by website :
+          <span class="data-usages-value">
+            ${((visitedArrayData[i].value) / 1024).toFixed(2)}
+          </span><span>
+
+            KB
+          </span>
+        </div>
+        <div class="carbon-emisson">
+          Carbon emission equivalent to :
+          <span class="carbon-emisson-value">
+          ${((visitedArrayData[i].value) / 95325).toFixed(2)}
+          </span><span>
+            gm
+          </span>
+        </div>
+      </div>
+    </li>`
+      if (count == 3) break;
+    }
+  }
+  template += `
+  <li class="view-more">
+  <a href="#">View More</a>
+  </li>`
+  const websites = document.getElementById("websites");
+  websites.innerHTML = template;
 });

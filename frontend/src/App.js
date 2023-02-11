bc8c3d070bad2c788e172e78dcba799312c1bbe4
unknown <aneesh0224@gmail.com>
2023-02-11 19:52:12 +0530

    added basic components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Navbar from './component/Navbar'
+import Home from './pages/Home/home'
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+      <Route  path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

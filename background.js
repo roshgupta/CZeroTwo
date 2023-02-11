4ad97f447efe21f98a9040bc1fc49f98147ea12d
unknown <aneesh0224@gmail.com>
2023-02-11 12:45:41 +0530

    made content length to be displayed

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+let tot=0
+
 // chrome.tabs.onUpdated.addListener((tabId, tab) => {
 //   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
 //     console.log(tabs[0].url);
@@ -5,7 +7,16 @@
 // });
 chrome.webRequest.onCompleted.addListener(
   function (details) {
-    console.log(details);
+    // console.log(details);
+
+    for(let it of details.responseHeaders){
+      if(it.name==='content-length'){
+        console.log(it.value)
+        tot=tot+parseInt(it.value);
+      }
+    }
+    console.log("gozaaa ",tot)
+
   },
   { urls: ["<all_urls>"] },
   ["responseHeaders"]

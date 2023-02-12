312b5ea4d6e01cc820647536aac8f884fe9d75b9
Roshan Gupta <gpta.duke.rohan.1219@gmail.com>
2023-02-12 07:52:36 +0530

    Final commit

--- backend/routes/userlink.js
+++ backend/routes/userlink.js
@@ -1,174 +1,173 @@
-const express=require('express')
-const router= express.Router()
-const User=require('../models/user')
-const Userlink=require('../models/userlink')
-const Link=require('../models/Link')
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+const Userlink = require('../models/userlink')
+const Link = require('../models/Link')
 
-const isAuthorised=require('../utils/isAuthorised')
+const isAuthorised = require('../utils/isAuthorised')
 
-router.get('/test',isAuthorised,(req,res,next)=>{
+router.get('/test', isAuthorised, (req, res, next) => {
     console.log(req.user.id)
     res.send("test")
 })
-router.get('/dum',(req,res)=>{
+router.get('/dum', (req, res) => {
     console.log("inside dum get")
     res.send("hi")
 })
 
-router.post('/dum',(req,res)=>{
+router.post('/dum', (req, res) => {
     console.log(req.body)
     console.log("inside dum post")
-    res.json({message:"h1"})
+    res.json({ message: "h1" })
 })
 
-router.get('/all',isAuthorised,async(req,res,next)=>{
-    try{
-    
-    const id=req.user.id
-    const user=await User.findById(id)
-    const ulinkid=user.userlink.toHexString()
-    const userlink=await Userlink.findById(ulinkid)
-    const visited=userlink.visit
-    const arr=[]
+router.get('/all', isAuthorised, async (req, res, next) => {
+    try {
 
-    for(let vis of visited){
-        const link=await Link.findById(vis.toHexString())
-        console.log(link)
-        arr.push(link)
+        const id = req.user.id
+        const user = await User.findById(id)
+        const ulinkid = user.userlink.toHexString()
+        const userlink = await Userlink.findById(ulinkid)
+        const visited = userlink.visit
+        const arr = []
+
+        for (let vis of visited) {
+            const link = await Link.findById(vis.toHexString())
+            console.log(link)
+            arr.push(link)
+        }
+
+        res.status(200).json({ success: true, payload: arr })
     }
-
-    res.status(200).json({success:true,payload:arr})
-}
-catch(err){
-    next(err)
-}
-})
-
-router.post('/',async(req,res,next)=>{
-    try{
-    // const id=req.user.id
-    const id="63e826018c0ddd53dfe1f463"
-    const user=await User.findById(id);
-    console.log(user)
-    const ulinkid=user.userlink.toHexString()
-    const userlink= await Userlink.findById(ulinkid);
-    // change here later
-    const {visited}= req.body
-    //visited here is an array
-    console.log("workingggg post")
-    const linkbody=await Link.create({visited})
-
-    userlink.visit.push(linkbody._id)
-    await userlink.save();
-
-    res.status(200).json({success:true,message:"done"})
-    }
-    catch(err){
+    catch (err) {
         next(err)
     }
 })
 
-router.get('/date/:date',isAuthorised,async(req,res,next)=>{
-    try{
-    const {date}=req.params;
-    const id=req.user.id
-    const user=await User.findById(id)
-    const ulinkid=user.userlink.toHexString()
-    const userlink=await Userlink.findById(ulinkid)
+router.post('/', isAuthorised, async (req, res, next) => {
+    try {
+        const id = req.user.id
+        const user = await User.findById(id);
+        console.log(user)
+        const ulinkid = user.userlink.toHexString()
+        const userlink = await Userlink.findById(ulinkid);
+        // change here later
+        const { visited } = req.body
+        //visited here is an array
+        console.log("workingggg post")
+        const linkbody = await Link.create({ visited })
 
-    const visited=userlink.visit
-   
-    for(let vis of visited){
+        userlink.visit.push(linkbody._id)
+        await userlink.save();
 
-        const link=await Link.findById(vis.toHexString())
-        console.log(link.date)
-        // if(link.date===date){
-        //     res.status(200).json({"success":true,payload:link})
-        // }
-
+        res.status(200).json({ success: true, message: "done" })
     }
-    res.status(200).json({success:false,message:"No such data"})
-}
-catch(err){
-    next(err)
-}
+    catch (err) {
+        next(err)
+    }
+})
+
+router.get('/date/:date', isAuthorised, async (req, res, next) => {
+    try {
+        const { date } = req.params;
+        const id = req.user.id
+        const user = await User.findById(id)
+        const ulinkid = user.userlink.toHexString()
+        const userlink = await Userlink.findById(ulinkid)
+
+        const visited = userlink.visit
+
+        for (let vis of visited) {
+
+            const link = await Link.findById(vis.toHexString())
+            console.log(link.date)
+            // if(link.date===date){
+            //     res.status(200).json({"success":true,payload:link})
+            // }
+
+        }
+        res.status(200).json({ success: false, message: "No such data" })
+    }
+    catch (err) {
+        next(err)
+    }
 })
 
 
-router.get('/top',isAuthorised,async(req,res,next)=>{
-    try{
-    let tot=0;
-    const id=req.user.id
-    const user=await User.findById(id)
-    const ulinkid=user.userlink.toHexString()
-    const userlink=await Userlink.findById(ulinkid)
+router.get('/top', isAuthorised, async (req, res, next) => {
+    try {
+        let tot = 0;
+        const id = req.user.id
+        const user = await User.findById(id)
+        const ulinkid = user.userlink.toHexString()
+        const userlink = await Userlink.findById(ulinkid)
 
-    const visited=userlink.visit
+        const visited = userlink.visit
 
-    const arr=[{url:"home page",value:0,carbon:0}]
-    for(let vis of visited){
+        const arr = [{ url: "home page", value: 0, carbon: 0 }]
+        for (let vis of visited) {
 
-        const link=await Link.findById(vis.toHexString())
-        const vis_arr=link.visited
-        for(let it of vis_arr){
-            tot=parseInt(tot)+parseInt(it.value)
-            let f=1;
-            for(let ind of arr){
-                if(ind.url===it.url){
-                    f=0;
-                    ind.value=parseInt(ind.value)+parseInt(it.value)
-                    ind.carbon=parseInt(ind.carbon)+parseInt(it.carbon)
-                    break;
+            const link = await Link.findById(vis.toHexString())
+            const vis_arr = link.visited
+            for (let it of vis_arr) {
+                tot = parseInt(tot) + parseInt(it.value)
+                let f = 1;
+                for (let ind of arr) {
+                    if (ind.url === it.url) {
+                        f = 0;
+                        ind.value = parseInt(ind.value) + parseInt(it.value)
+                        ind.carbon = parseInt(ind.carbon) + parseInt(it.carbon)
+                        break;
+                    }
+                }
+                if (f == 1) {
+                    arr.push(it);
                 }
             }
-            if(f==1){
-                arr.push(it);
-            }
         }
+
+        arr.sort((a, b) => parseInt(a.value) > parseInt(b.value) ? -1 : 1);
+
+        res.status(200).json({ arr, tot })
     }
-
-    arr.sort((a, b) => parseInt(a.value) > parseInt(b.value) ? -1 : 1);
-
-    res.status(200).json({arr,tot})
-}
-catch(err){
-    next(err)
-}
+    catch (err) {
+        next(err)
+    }
 })
 
-router.get('/:url',isAuthorised,async(req,res,next)=>{
-try{
-    const {url}=req.params
-    console.log(url)
-    const id=req.user.id
-    const user=await User.findById(id)
-    const ulinkid=user.userlink.toHexString()
-    const userlink=await Userlink.findById(ulinkid)
+router.get('/:url', isAuthorised, async (req, res, next) => {
+    try {
+        const { url } = req.params
+        console.log(url)
+        const id = req.user.id
+        const user = await User.findById(id)
+        const ulinkid = user.userlink.toHexString()
+        const userlink = await Userlink.findById(ulinkid)
 
-    const visited=userlink.visit
+        const visited = userlink.visit
 
-    let value=0;
-    let carbon=0;
+        let value = 0;
+        let carbon = 0;
 
-    for(let vis of visited){
-        const link=await Link.findById(vis.toHexString())
-        const vis_arr=link.visited
-        for(let it of vis_arr){
-            if(it.url===url){
-                value=parseInt(value)+parseInt(it.value)
-                carbon=parseInt(carbon)+parseInt(it.carbon)
+        for (let vis of visited) {
+            const link = await Link.findById(vis.toHexString())
+            const vis_arr = link.visited
+            for (let it of vis_arr) {
+                if (it.url === url) {
+                    value = parseInt(value) + parseInt(it.value)
+                    carbon = parseInt(carbon) + parseInt(it.carbon)
+                }
+
             }
 
         }
-
+        res.status(200).json({ success: true, value, carbon, url })
     }
-    res.status(200).json({success:true,value,carbon,url})
-}
-catch(err){
-    next(err)
-}
+    catch (err) {
+        next(err)
+    }
 
 })
 
 
-module.exports= router
+module.exports = router
